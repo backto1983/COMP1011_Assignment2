64be7823eb0a85fbf35e965424d86178d7abfe54
backto1983 <200358165@student.georgianc.on.ca>
2018-04-10 14:13:45 -0400

    Book class

--- src/comp1011_assigment2/Book.java
+++ src/comp1011_assigment2/Book.java
@@ -1,5 +1,8 @@
 package comp1011_assigment2;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  *
  * @author Henrique
@@ -24,15 +27,17 @@ public class Book {
             this.bookID = bookID;
         else
             throw new IllegalArgumentException("Book ID must be equal or greather than 0");
-    }
-    
+    }    
 
     public int getYear() {
         return year;
     }
 
     public void setYear(int year) {
-        this.year = year;
+        if(year >= 1900 || year <= 2018)        
+            this.year = year;
+        else
+            throw new IllegalArgumentException("This library only accepts books that were published since 1900");
     }
 
     public String getTitle() {
@@ -40,7 +45,10 @@ public class Book {
     }
 
     public void setTitle(String title) {
-        this.title = title;
+        if (!title.isEmpty())
+            this.title = title;
+        else
+            throw new IllegalArgumentException("Title cannot be empty");        
     }
 
     public String getAuthor() {
@@ -48,7 +56,17 @@ public class Book {
     }
 
     public void setAuthor(String author) {
-        this.author = author;
+        if (!author.isEmpty())
+            this.author = author;
+        else
+            throw new IllegalArgumentException("Author cannot be empty");       
+    }
+    
+    public static List<String> getGenres()
+    {
+      String[] genres = {"Adventure", "Biography", "Business", "Crime", "Fantasy" , "Health", "History", "Humour", 
+          "Political", "Romance", "Sci-Fi", "Self-Help", "Sports", "Technology", "Terror"};
+      return Arrays.asList(genres);
     }
 
     public String getGenre() {
@@ -56,7 +74,10 @@ public class Book {
     }
 
     public void setGenre(String genre) {
-        this.genre = genre;
+        if(getGenres().contains(genre))        
+            this.genre = genre;
+        else
+            throw new IllegalArgumentException("Valid genres are: " + getGenres());
     }
     
     
